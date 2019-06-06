9a4d6b991e8362fc3978e3bebe520f5f7e2ecb00
Taehan <48433442+TaeHan-Ka@users.noreply.github.com>
2019-06-06 12:15:16 +0900

    Update MainActivity.java

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,101 +1,124 @@
 package com.example.myapplication;
 
-import android.content.ContentValues;
-import android.content.Context;
-import android.content.Intent;
+
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteException;
-import android.database.sqlite.SQLiteOpenHelper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
+import java.util.ArrayList;
+import java.util.HashMap;
 
-public class MainActivity  extends AppCompatActivity implements View.OnClickListener{
-    SQLiteDatabase db;
-    dbHelper helper;
-    EditText no;
-    Cursor cursor;
-//    MyCursorAdapter myAdapter;
+public class Numlist  extends AppCompatActivity {
 
+    EditText phone;
+    EditText name;
+    private ArrayList<HashMap<String,String>> Data = new ArrayList<HashMap<String, String>>();
+    private HashMap<String,String> InputData1 = new HashMap<>();
+    private HashMap<String,String> InputData2 = new HashMap<>();
+
+
+    ListView list;
+    ListAdapter adapter;
+    ArrayList<HashMap<String, String>> personList;
+
+    private final String DBname = "Test";
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.numlist);
+        name = (EditText) findViewById(R.id.note);
+        phone = (EditText) findViewById(R.id.note2);
+        final TextView result = (TextView) findViewById(R.id.result);
+        list = (ListView) findViewById(R.id.list);
+        personList = new ArrayList<HashMap<String,String>>();
+
+        final DBHelper dbHelper = new DBHelper(getApplicationContext(), DBname, null, 1);
+        result.setText(dbHelper.getResult());
+        listView();
 
 
-        helper = new dbHelper(this);
+        Button insert = (Button) findViewById(R.id.insert);   // 데이터 넣기
+        insert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String item = name.getText().toString();
+                String price = phone.getText().toString();
+
+                dbHelper.insert(item, price);
+                result.setText(dbHelper.getResult());
+                listView();
+                name.setText("");
+                phone.setText("");
+            }
+        });
+
+        Button delete = (Button) findViewById(R.id.delete);   // 삭제
+        delete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String item = name.getText().toString();
+                dbHelper.delete(item);
+                result.setText(dbHelper.getResult());
+                listView();
+            }
+        });
+
+
+        // DB에 있는 데이터 수정
+        Button update = (Button) findViewById(R.id.update);     // 고치기
+        update.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String item = name.getText().toString();
+                String price = phone.getText().toString();
+                dbHelper.update(item, price);
+                result.setText(dbHelper.getResult());
+                listView();
+            }
+        });
+    }
+
+    public void listView(){
         try {
-            db = helper.getWritableDatabase();
-            //데이터베이스 객체를 얻기 위하여 getWritableDatabse()를 호출
-        } catch (SQLiteException e) {
-            db = helper.getReadableDatabase();
+            SQLiteDatabase dbHelper1 = this.openOrCreateDatabase(DBname, 0, null);
+            Cursor c = ((SQLiteDatabase) dbHelper1).rawQuery("SELECT * FROM MONEYBOOK", null);
+            personList.clear();
+            if (c != null) {
+
+                if (c.moveToFirst()) {
+                    do {
+                        String n = c.getString(c.getColumnIndex("name"));
+                        String p = c.getString(c.getColumnIndex("phone"));
+
+                        HashMap<String, String> persons = new HashMap<String, String>();
+
+                        persons.put("name", n);
+                        persons.put("phone", p);
+
+                        personList.add(persons);
+
+                    } while (c.moveToNext());
+                }
+            }
+            dbHelper1.close();
+            adapter = new SimpleAdapter(
+                    this, personList, R.layout.list_item,
+                    new String[]{"name","phone"},
+                    new int[]{ R.id.name, R.id.phone}
+            );
+            list.setAdapter(adapter);
+        }catch (SQLiteException se) {
+            Toast.makeText(getApplicationContext(),  se.getMessage(), Toast.LENGTH_LONG).show();
+            Log.e("",  se.getMessage());
         }
-
-    }
-
-    @Override
-    public void onClick(View view) {
-        switch (view.getId()){
-            case R.id.insert:
-                no = (EditText)findViewById(R.id.note2);
-                String NO = no.getText().toString();
-                ContentValues value = new ContentValues();
-                value.put("test", Integer.valueOf(NO));
-                db.insert("test",null,value);
-
-                /*Intent intent = new Intent(getApplicationContext(),MainActivity.class);   // 완료 후 다음 위치
-                startActivity(intent);*/
-                no.setText(""); // 텍스트 초기화
-        }
     }
 }
-
-class dbHelper extends SQLiteOpenHelper {
-
-    private static final String DATABASE_NAME = "test";
-    private static final int DATABASE_VERSION = 1;
-
-    /*
-     *먼저 SQLiteOpenHelper클래스를 상속받은 dbHelper클래스가 정의 되어 있다. 데이터베이스 파일 이름은 "mycontacts.db"가되고,
-     *데이터베이스 버전은 1로 되어있다. 만약 데이터베이스가 요청되었는데 데이터베이스가 없으면 onCreate()를 호출하여 데이터베이스
-     *파일을 생성해준다.
-     */
-    public dbHelper(Context context) {
-        super(context, DATABASE_NAME, null, DATABASE_VERSION);
-    }
-
-    @Override
-    public void onCreate(SQLiteDatabase db) {
-        db.execSQL("CREATE TABLE test (_id INTEGER PRIMARY KEY AUTOINCREMENT, test TEXT);");
-    }
-
-    @Override
-    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        db.execSQL("DROP TABLE IF EXITS test");
-        onCreate(db);
-    }
-}
-/*
-
-class MyCursorAdapter extends CursorAdapter {
-    @SuppressWarnings("deprecation")
-    public MyCursorAdapter(Context context, Cursor c) {
-        super(context, c);
-    }
-    @Override
-    public void bindView(View view, Context context, Cursor cursor) {
-        TextView tvName = (TextView) view.findViewById( R.id.tv_name );
-        TextView tvAge = (TextView) view.findViewById( R.id.tv_age );
-        String name = cursor.getString( cursor.getColumnIndex( KEY_NAME ) );
-        String age = cursor.getString( cursor.getColumnIndex( KEY_AGE ) );
-        Log.d("스트링 확인", name + ", " + age); tvName.setText( name ); tvAge.setText( age );
-    }
-    @Override
-    public View newView(Context context, Cursor cursor, ViewGroup parent) {
-        LayoutInflater inflater = LayoutInflater.from( context );
-        View v = inflater.inflate( R.layout.list_item, parent, false ); return v;
-    }
-}
-*/

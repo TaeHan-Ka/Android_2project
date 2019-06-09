146c14b357ddaf37b64d5933d526438aff381b17
Taehan <48433442+TaeHan-Ka@users.noreply.github.com>
2019-06-10 02:12:04 +0900

    Update sms.java

--- app/src/main/java/com/example/myapplication/sms.java
+++ app/src/main/java/com/example/myapplication/sms.java
@@ -1,7 +1,10 @@
 package com.example.myapplication;
 
-import android.content.Intent;
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.telephony.SmsManager;
 import android.view.View;
@@ -9,23 +12,51 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-public class sms  extends AppCompatActivity implements View.OnClickListener {
+
+public class sms  extends AppCompatActivity {
     Button buttonSend;
     EditText textPhoneNo;
     EditText textSMS;
+    static final Main M = new Main();
+    static final int SMS_RECEIVE_PERMISSON=1;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sms);
 
-        buttonSend = (Button) findViewById(R.id.send);
         textPhoneNo = (EditText) findViewById(R.id.editTextPhoneNo);
         textSMS = (EditText) findViewById(R.id.editTextSMS);
+        String pNum = ((Main)Main.context).pp;
+        //pNum = M.getPp();
+        Toast.makeText(getBaseContext(), pNum, Toast.LENGTH_SHORT).show();
+        textPhoneNo.setText("0"+pNum);
+        int permissonCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.RECEIVE_SMS);
+        if(permissonCheck == PackageManager.PERMISSION_GRANTED){     //  퍼미션 확인
+            Toast.makeText(getApplicationContext(), "SMS 수신권한 있음", Toast.LENGTH_SHORT).show();
+        }else{      // 권한 없을 시
+            Toast.makeText(getApplicationContext(), "SMS 수신권한 없음", Toast.LENGTH_SHORT).show();
 
-    }
-    public void onClick(View view) {
-        switch (view.getId()){
-            case R.id.send:
+            //권한설정 dialog에서 거부를 누르면
+            //ActivityCompat.shouldShowRequestPermissionRationale 메소드의 반환값이 true가 된다.
+            //단, 사용자가 "Don't ask again"을 체크한 경우
+            //거부하더라도 false를 반환하여, 직접 사용자가 권한을 부여하지 않는 이상, 권한을 요청할 수 없게 된다.
+            if(ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.RECEIVE_SMS)){
+                // 권한을 요청한다.
+                Toast.makeText(getApplicationContext(), "SMS권한이 필요합니다", Toast.LENGTH_SHORT).show();
+                ActivityCompat.requestPermissions(this, new String[]{ Manifest.permission.RECEIVE_SMS}, SMS_RECEIVE_PERMISSON);
+            }else{
+                ActivityCompat.requestPermissions(this, new String[]{ Manifest.permission.RECEIVE_SMS}, SMS_RECEIVE_PERMISSON);
+            }
+        }
+
+
+        buttonSend = (Button) findViewById(R.id.send);
+        buttonSend.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                //입력한 값을 가져와 변수에 담는다
+
                 String phoneNo = textPhoneNo.getText().toString();
                 String sms = textSMS.getText().toString();
                 try {
@@ -37,10 +68,20 @@ public class sms  extends AppCompatActivity implements View.OnClickListener {
                     Toast.makeText(getApplicationContext(), "SMS faild, please try again later!", Toast.LENGTH_LONG).show();
                     e.printStackTrace();
                 }
-                Intent intent = new Intent(getApplicationContext(),Main.class);   // 완료 후 다음 위치
-                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int grantResults[]){
+        switch(requestCode){
+            case SMS_RECEIVE_PERMISSON:
+                if(grantResults.length>0 && grantResults[0]==PackageManager.PERMISSION_GRANTED){
+                    Toast.makeText(getApplicationContext(), "SMS권한 승인함", Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(getApplicationContext(), "SMS권한 거부함", Toast.LENGTH_SHORT).show();
+                }
+                break;
         }
-//                Intent intent = new Intent(getApplicationContext(),DB.class);   // 완료 후 다음 위치
-//                startActivity(intent);
     }
 }
